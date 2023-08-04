5fdadb06d493e4820aabcf85d6b321a15b7d72c1
angelamyungjoosong <angelaforever122@gmail.com>
2023-08-04 18:23:13 +0900

    first commit

---
+++ codes/mysql/select.py
@@ -0,0 +1,19 @@
+# mysql connect package
+# pip (퓨어자바에서 스토어를 연결하는 명령어) 대신 conda 사용 
+
+# connect Mysql
+
+# ip, port, username, password, database name : 연결할 때 필요한 5가지 
+
+# editor화면 - statement 
+
+# make select query 
+
+# execute select query 
+
+#return resultset and then display
+
+#close 
+
+
+
